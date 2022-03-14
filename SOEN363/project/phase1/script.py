361e6d7845dd70117598d6e61428da448e764954
Ali Hanni <alihanni95@gmail.com>
2022-03-13 21:23:21 -0400

    Completed part 2: execution time analysis with indexes

--- SOEN363/project/phase1/script.py
+++ SOEN363/project/phase1/script.py
@@ -5,6 +5,7 @@ files = listdir('./execution')
 
 for file in files:
     path = './results/' + '-'.join(file.split('-', 2)[:2])
+    print(file)
     df = pd.read_csv(f'./execution/{file}')
     execution_time = df.iloc[len(df) - 1].values[0]
 
